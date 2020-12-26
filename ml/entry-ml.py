d828dee60cd0a5d68deb38b592c1bebd05f71b35
Alex Rosen <alex.rosen@tessian.com>
2020-12-26 22:04:46 +0100

    x

---
+++ ml/entry-ml.py
@@ -0,0 +1,34 @@
+import csv
+import random
+import re
+import sys
+
+import Levenshtein
+
+
+# load our dicts
+from collections import Counter, defaultdict
+
+import nltk
+
+NONLETTERS = re.compile("[^a-z ]")
+
+lines = open("/Users/alex.rosen/personal/xword/corpora/matt-ginsberg-clues.txt", encoding='latin1').readlines()
+lines = [line.rstrip().lower().split(",", maxsplit=1)[1] for line in lines]
+lines = [NONLETTERS.sub("", line) for line in lines]
+lines = set([line for line in lines if not ("_" in line or "--" in line)])
+clues = [line.split() for line in lines]
+
+with open("/Users/alex.rosen/personal/projects/misc/specific/clues-for-ml-experiment.txt", "w") as f:
+
+    words = []
+    for clue in clues:
+        words.extend(clue)
+        print("__label__1 " + " ".join(clue), file = f)
+
+    words = list(words)
+    for clue in clues:
+        fake_clue = random.choices(words, k=len(clue))
+        print("__label__0 " + " ".join(fake_clue), file = f)
+
+
